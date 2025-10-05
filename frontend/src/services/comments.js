7982f1cb099d4d3bf9fc0c72162595cbcee6f1af
rahul7697762 <sainirahul5087@gmail.com>
2025-10-05 19:28:00 +0530

    change name src to dist

--- frontend/src/services/comments.js
+++
@@ -1,9 +0,0 @@
-import { apiGet, apiPost } from './api.js'
-
-export function listComments(ticketId){
-  return apiGet(`/api/comments/ticket/${ticketId}`)
-}
-
-export function addComment(data){
-  return apiPost('/api/comments', data)
-}
